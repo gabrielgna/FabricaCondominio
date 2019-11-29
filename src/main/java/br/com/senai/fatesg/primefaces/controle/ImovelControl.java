484c51e9952822756a7ee8d106370e0170633181
Jorge <jorgematheus.p@gmail.com>
2019-11-28 23:54:59 -0300

    TelasEPersistencia Imovel

---
+++ src/main/java/br/com/senai/fatesg/primefaces/controle/ImovelControl.java
@@ -0,0 +1,68 @@
+package br.com.senai.fatesg.primefaces.controle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.event.ActionEvent;
+
+import javax.inject.Named;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+
+import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
+
+import br.com.senai.fatesg.primefaces.entidade.Imovel;
+import br.com.senai.fatesg.primefaces.persistencia.ImovelDao;
+
+@Named("ImovelControl")
+@Scope("conversationImovel")
+public class ImovelControl {
+
+	private Imovel imovel = new Imovel();
+
+	@Autowired
+	private ImovelDao imoveisDao;
+
+	private List<Imovel> imoveis = new ArrayList<Imovel>();
+
+	@PostConstruct
+	public void init() {
+		listar(null);
+	}
+
+	public void listar(ActionEvent evt) {
+		try {
+			imoveis = imoveisDao.listar();
+		} catch (Exception e) {
+			UtilFaces.addMensagemFaces(e);
+		}
+	}
+
+	public void confirmar(ActionEvent evt) {
+		try {
+			imoveisDao.alterar(imovel);
+			listar(evt);
+			imovel = new Imovel();
+		} catch (Exception e) {
+			UtilFaces.addMensagemFaces(e);
+		}
+	}
+
+
+	public Imovel getImovel() {
+		return imovel;
+	}
+
+	public void setImovel(Imovel imovel) {
+		this.imovel = imovel;
+	}
+
+	public List<Imovel> getImoveis() {
+		return imoveis;
+	}
+
+	
+
+}
